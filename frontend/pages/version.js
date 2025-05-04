ec3c0fb5893ad74b305d5a7c692ba329772bfa0e
dmk1en <duongkien50@gmail.com>
2025-05-04 10:14:48 +0700

    fix bug

--- frontend/pages/version.js
+++ frontend/pages/version.js
@@ -1,11 +1,22 @@
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-export default async function handler(req, res) {
-  try {
-    const response = await axios.get('http://localhost:4000/api/version');
-    res.status(200).send(response.data.version); // Send the version as plain text
-  } catch (error) {
-    console.error('Error fetching version:', error.message);
-    res.status(500).send('Failed to fetch version'); // Send error as plain text
-  }
+export default function Version() {
+  const [version, setVersion] = useState('');
+
+  useEffect(() => {
+    const fetchVersion = async () => {
+      try {
+        const response = await axios.get('http://localhost:4000/api/version');
+        setVersion(response.data.version);
+      } catch (error) {
+        console.error('Error fetching version:', error.message);
+        setVersion('Error fetching version');
+      }
+    };
+
+    fetchVersion();
+  }, []);
+
+  return <>{version}</>;
 }
